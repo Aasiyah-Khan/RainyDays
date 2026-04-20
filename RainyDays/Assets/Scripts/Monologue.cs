d4ef9955a88b9e4f5c0ec2f26ebc4285205005e4
Aasiyah-Khan <139803670+Aasiyah-Khan@users.noreply.github.com>
2026-04-20 13:58:56 -0400

    I AM AN IIDIOT
    monolgue implemented and pushed... never copy and paste code even if it looks repetetive i wasted over an hour trying to see where i messed up

--- RainyDays/Assets/Scripts/Monologue.cs
+++ RainyDays/Assets/Scripts/Monologue.cs
@@ -16,6 +16,10 @@ public class Monologue : MonoBehaviour
 
     // the array of logs
     string[] comments;
+    string[] dogComments;
+    string[] monsterComments;
+    string[] guitarComments;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,12 +27,42 @@ public class Monologue : MonoBehaviour
         // hide the UI
         UI.SetActive(false);
         UIText.SetActive(false);
+        // set up for the monologue
         comments = new string[5]; 
         comments[0] = "I touched the box!";
         comments[1] = "Box has been touched!";
         comments [2] = "This box is purple!";
         comments[3] = "I like boxes!";
         comments[4] = "Okay ran out of ideas!";
+
+        // Im a fricken idiot and will never copy and paste again i wasted a fricken hour trying to figure out what was wrong and my slow brain finally got it
+        dogComments = new string[4]; 
+        dogComments[0] = "I'm gonna miss you, bud!";
+        dogComments[1] = "Stop looking at me like that; I have no food.";
+        dogComments [2] = "You're the only one that gets me.";
+        dogComments[3] = "I wish I was as carefree as you.";
+
+        monsterComments = new string[3]; 
+        monsterComments[0] = "I'm gonna make this can last the whole ride.";
+        monsterComments[1] = "I should've brought two cans.";
+        monsterComments [2] = "I wish I had another monster.";
+      
+        guitarComments = new string[4]; 
+        guitarComments[0] = "The last gig we had was lit.";
+        guitarComments[1] = "I remember getting that sticker from Comic Con.";
+        guitarComments [2] = "Maybe I'll make a friend who also plays.";
+        guitarComments [3] = "I wonder if the band will ever play together again...";
+      
+
+
+
+        
+
+        
+
+
+
+
         // so you can't keep spamming interactions
         touched = false;
 
@@ -71,7 +105,7 @@ public class Monologue : MonoBehaviour
         foreach (char letter in sentence.ToCharArray())
         {
             logs += letter;
-             UIText.GetComponent<TextMeshProUGUI>().text = logs;
+            UIText.GetComponent<TextMeshProUGUI>().text = logs;
             yield return new WaitForSeconds(0.02f);
         }
     }
@@ -85,16 +119,46 @@ public class Monologue : MonoBehaviour
         StartCoroutine(UIDisplay(message));
     }
 
+    // another helper method cuz I aint repeating myself
+    public void Touched(string[] comments, int commentsLength)
+    {
+                // choose a random number
+                int randoNumbo = UnityEngine.Random.Range(0, commentsLength);
+                logs = comments[randoNumbo];
+                // run the helper function
+                Display(logs);
+                
+    }
+
     public void Interaction()
     {
         if(touched == false)
         { 
             touched = true;
-            // choose a random number
-            int randoNumbo = UnityEngine.Random.Range(0,5);
-            logs = comments[randoNumbo];
-            // run the helper function
-            Display(logs);
+            if(this.gameObject.name == "Dog Button")
+            {
+                Debug.Log("Dog clicked");
+               
+                Touched(dogComments, dogComments.Length);
+            }
+            else if(this.gameObject.name == "Monster")
+            {
+                Debug.Log("Monster clicked");
+                
+                Touched(monsterComments, monsterComments.Length);
+            }
+            else if(this.gameObject.name == "getar")
+            {
+                 Debug.Log("Guitar clicked");
+                 
+                Touched(guitarComments, guitarComments.Length);
+            }
+            else
+            {
+               
+                Touched(comments, comments.Length);
+            }
+            
            
         }
         else
